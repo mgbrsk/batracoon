f0493e740d21fbfd132f7909e429ebb303b90c3e
fizteh95 <causeLove95@yandex.ru>
2021-03-19 23:31:25 +0500

    Гавно, надо переделывать.

--- simple.py
+++ simple.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 В этом файлк буду реализовывать подход из методички Шумилова
 '''
@@ -30,30 +31,72 @@ print('Hello world!')
 #     def tik(self):
 #         pass
 
-neurons = [[0, 0],
-           [1, 0],
-           [2, 0],
-           [3, 0],
-           [4, 0],
-           [5, 0],
-           [6, 0],
-           [7, 0],
-           [8, 0],
-           [9, 0]]  # айдишники нейронов, активирован ли
+import random
 
-synapses = [[0, 1, 0.5, 0],  # откуда, куда, вес, есть ли импульс в синапсе
+
+neurons = [[0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 0],
+           [2, 0, 0, 0, 0],
+           [3, 0, 0, 0, 0],
+           [4, 0, 0, 0, 0],
+           # [5, 0, 0, 0],
+           # [6, 0, 0, 0],
+           # [7, 0, 0, 0],
+           # [8, 0, 0, 0],
+           [9, 0, 0, 0, 0]]  # айдишники нейронов, активирован ли, x, y, аккумулятор
+
+synapses = [[0, 1, 0.5, 0],  # откуда, куда, вес, активировался ли в этом тике
             [2, 1, 0.5, 0],
-            [2, 9, 1, 0]]
+            [1, 4, 1, 0]]
 
-probes = []  # [[0, 1], [0, 2], [1, 2], ...] на какие нейроны над подать сигнал
+probes = [[0, 2], [0, 0], [0, 0], [0, 0]]  # [[0, 1], [0, 2], [1, 2], ...] на какие нейроны над подать сигнал
 
+
+for n in neurons:
+    n[2], n[3] = random.random(), random.random()
+
+
+# TODO: нужна проверка нейронов на активацию в этом тике?
+# TODO: проверка на сумму весов на входе
 for item in probes:
+    for s in synapses:
+        s[3] = 0
+    for s in synapses:
+        first_neuron = s[0]
+        second_neuron = s[1]
+        # активируем синапсы
+        if (neurons[first_neuron][1] == 1) and (s[3] == 0):
+            neurons[second_neuron][4] += s[2]
+            neurons[first_neuron][1] = 0
+            s[3] = 1
+            if neurons[second_neuron][4] >= 1:
+                neurons[first_neuron][1] = 1
+            else:
+                neurons[first_neuron][1] = 0
+
+    # for s in synapses:
+    #     first_neuron = s[0]
+    #     second_neuron = s[1]
+    #     if s[3] == 1:
+
+
     # возбуждаем нейроны входными импульсами
     for p in item:
-        neurons[p] = [p, 1]
+        neurons[p] = [p, 1, neurons[p][2], neurons[p][3], neurons[p][4]]
 
-    for s in synapses:
-        pass
+    if neurons[4][1] == 1:
+        print('yeah!')
+
+    for n in neurons:
+        if n[1] == 0:
+            n[4] = 0
+        if n[1] == 1:
+            zroid = True
+            for s in synapses:
+                if s[0] == n[0]:
+                    zroid = False
+        if zroid:
+            n[1] = 0
 
 
 
