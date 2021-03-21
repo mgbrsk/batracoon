9af05490071bb2c5a8ea2128d490339b49b612a0
fizteh95 <causeLove95@yandex.ru>
2021-03-21 21:24:06 +0500

    Переделал. Прямое распространение.

--- simple.py
+++ simple.py
@@ -1,111 +1,104 @@
 # -*- coding: utf-8 -*-
-'''
-В этом файлк буду реализовывать подход из методички Шумилова
-'''
-
-print('Hello world!')
 
 
-# class Neuron:
-#     def __init__(self, p_excited=0.01, p_join=0.9, koef_old=0.5):
-#         self.p_excited = p_excited
-#         self.p_join = p_join
-#         self.koef_old = koef_old
-#         self.input_weights = []
+class Synapse:
+    def __init__(self, input_n, output_n, weight=0.5):
+        self.input_n = input_n
+        self.output_n = output_n
+        self.weight = weight
+        self.is_signal = 0
+
+    def check_signal(self):
+        if self.is_signal == 0:
+            return False
+        else:
+            self.is_signal = 0
+            return True
+
+    def activate(self):
+        self.is_signal = 1
 
 
-# class Net:
-#     def __init__(self, n_neurons=10, p_excited=0.01, p_join=0.9, koef_old=0.5):
-#         self.n_neurons = n_neurons
-#         self.p_excited = p_excited
-#         self.p_join = p_join
-#         self.koef_old = koef_old
+class Neuron:
+    def __init__(self, number, accumulator=0):
+        self.accumulator = accumulator
+        self.number = number
 
-#         self.synapse = []
-#         self.neurons = []
-#         for n in range(self.n_neurons):
-#             self.neurons.append(Neuron(p_excited=self.p_excited,
-#                                        p_join=self.p_join,
-#                                        koef_old=self.koef_old))
+    def adding(self, value):
+        self.accumulator += value
 
-#     def tik(self):
-#         pass
-
-import random
-
-
-neurons = [[0, 0, 0, 0, 0],
-           [1, 0, 0, 0, 0],
-           [2, 0, 0, 0, 0],
-           [3, 0, 0, 0, 0],
-           [4, 0, 0, 0, 0],
-           # [5, 0, 0, 0],
-           # [6, 0, 0, 0],
-           # [7, 0, 0, 0],
-           # [8, 0, 0, 0],
-           [9, 0, 0, 0, 0]]  # айдишники нейронов, активирован ли, x, y, аккумулятор
-
-synapses = [[0, 1, 0.5, 0],  # откуда, куда, вес, активировался ли в этом тике
-            [2, 1, 0.5, 0],
-            [1, 4, 1, 0]]
-
-probes = [[0, 2], [0, 0], [0, 0], [0, 0]]  # [[0, 1], [0, 2], [1, 2], ...] на какие нейроны над подать сигнал
+    def is_activated(self):
+        if self.accumulator >= 1:
+            self.accumulator = 0
+            return True
+        else:
+            self.accumulator -= 0.1
+            if self.accumulator <= 0:
+                self.accumulator = 0
+            return False
 
 
-for n in neurons:
-    n[2], n[3] = random.random(), random.random()
+class Net:
+    def __init__(self, n_neurons, synapses=None, output_n=None):
+        self.n_neurons = n_neurons
+        self.output_n = output_n
+        self.neurons = []
+        if not synapses:
+            self.synapses = []
+        else:
+            self.synapses = synapses
+
+        for i in range(self.n_neurons):
+            self.neurons.append(Neuron(i))
+
+    def add_synapse(self, input_n, output_n, weight=0.5):
+        s = Synapse(input_n, output_n, weight=weight)
+        self.synapses.append(s)
+
+    def get_neuron(self, number):
+        for n in self.neurons:
+            if n.number == number:
+                return n
+        else:
+            raise
+
+    def get_target_synapses(self, out):
+        result = []
+        for s in self.synapses:
+            if s.input_n == out:
+                result.append(s)
+        return result
+
+    def probe(self, number):
+        n = self.get_neuron(number)
+        n.accumulator += 1
+
+    def tik(self):
+        # проверяем сигнал в синапсах
+        # суммируем сигналы в целевых нейронах
+        for s in self.synapses:
+            if s.check_signal():
+                current_neuron = self.get_neuron(s.output_n)
+                current_neuron.adding(s.weight)
+
+        # >1 - активируем
+        # выдаем сигнал в синапс
+        for n in self.neurons:
+            if n.is_activated():
+                synapses_to_activate = self.get_target_synapses(n.number)
+                for item in synapses_to_activate:
+                    item.activate()
+                if n.number == self.output_n:
+                    print('hop')
 
 
-# TODO: нужна проверка нейронов на активацию в этом тике?
-# TODO: проверка на сумму весов на входе
-for item in probes:
-    for s in synapses:
-        s[3] = 0
-    for s in synapses:
-        first_neuron = s[0]
-        second_neuron = s[1]
-        # активируем синапсы
-        if (neurons[first_neuron][1] == 1) and (s[3] == 0):
-            neurons[second_neuron][4] += s[2]
-            neurons[first_neuron][1] = 0
-            s[3] = 1
-            if neurons[second_neuron][4] >= 1:
-                neurons[first_neuron][1] = 1
-            else:
-                neurons[first_neuron][1] = 0
-
-    # for s in synapses:
-    #     first_neuron = s[0]
-    #     second_neuron = s[1]
-    #     if s[3] == 1:
-
-
-    # возбуждаем нейроны входными импульсами
-    for p in item:
-        neurons[p] = [p, 1, neurons[p][2], neurons[p][3], neurons[p][4]]
-
-    if neurons[4][1] == 1:
-        print('yeah!')
-
-    for n in neurons:
-        if n[1] == 0:
-            n[4] = 0
-        if n[1] == 1:
-            zroid = True
-            for s in synapses:
-                if s[0] == n[0]:
-                    zroid = False
-        if zroid:
-            n[1] = 0
-
-
-
-# def probe_signal(neuron_ids):
-#     activated = []
-#     for n in neuron_ids:
-#         a[n] = [n, 1]
-#         activated.append(n)
-
-#     for t in a:
-#         if t[0] not in activated:
-#             t = [t[0], 0]
+synapses = [Synapse(0, 2, weight=0.5), Synapse(1, 2, weight=0.5), Synapse(2, 3, weight=1.5)]
+net = Net(5, synapses=synapses, output_n=3)
+net.probe(0)
+net.probe(1)
+for i in range(50):
+    net.tik()
+net.probe(0)
+net.probe(1)
+for i in range(50):
+    net.tik()
