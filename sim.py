6978ccac5fd2ee85a5500b54a0ab69604fa4575e
Arseniy Mironov <ars-mironov@yandex.ru>
2021-04-09 15:54:07 +0500

    Описание алгоритма с моей колокольни.

--- sim.py
+++ sim.py
@@ -3,16 +3,32 @@ from logging import info
 
 
 class Entity():
-    def __init__(self):
+    def __init__(self,
+                 sensor_neuron_amount,
+                 connect_neurons_amount,
+                 motor_neuron_amount):
         self.life_status: bool = True
         # self.doors = doors
         # self.tires = tires
+        self.sensor_neuron_amount = sensor_neuron_amount
+        self.connect_neurons_amount = connect_neurons_amount
+        self.motor_neuron_amount = motor_neuron_amount
 
     def kill(self):
         """Можно избавиться от организма.
 
         """
         self.life_status = False
+
+    def structure_info(self):
+        if self.life_status:
+            print(f'q_q{self.sensor_neuron_amount}'
+                  f'(__{self.connect_neurons_amount}__)'
+                  f'{self.motor_neuron_amount}~~')
+        else:
+            print(f'x_x{self.sensor_neuron_amount}'
+                  f'(__{self.connect_neurons_amount}__)'
+                  f'{self.motor_neuron_amount}--')
 
 
 class Neuron():
