fd84a4c16aad029aee134c251801770ae0c2d8da
fizteh95 <causeLove95@yandex.ru>
2021-04-19 21:58:06 +0500

    Исправил расчет cd/dw на матрицах на более понятный.

--- sasha/matrix_approach.py
+++ sasha/matrix_approach.py
@@ -1,78 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-# n_accumulator = np.zeros((5,))
-# n_signals = np.zeros((5,))
-# s_weights = np.array([[0, 0, 0, 0, 0],
-#                       [0, 0, 0, 4, 0],
-#                       [2, 3, 0, 0, 0],
-#                       [0, 0, -8, 0, 0],
-#                       [0, 0, 10, -2, 0]])
-# s_signals = np.zeros((5, 5))
-# s_real = np.array([[0, 0, 0, 0, 0],
-#                    [0, 0, 0, 0, 0],
-#                    [1, 0, 0, 0, 0],
-#                    [0, 1, 0, 0, 0],
-#                    [0, 0, 1, 1, 0]])
-#
-# n_accumulator[0] = 30
-# n_accumulator[1] = 15
-#
-# for _ in range(50):
-#     n_signals = np.where(n_accumulator > 1, 1, n_signals)
-#     temp = s_weights * s_real * n_signals
-#     n_accumulator = np.sum(temp, axis=1)  # n_accumulator + temp_sum
-#     if n_signals[4] > 0:
-#         print('hop')
-#     n_signals = np.zeros((5,))
-#
-# n_accumulator[0] = 30
-# n_accumulator[1] = 15
-#
-# for _ in range(50):
-#     n_signals = np.where(n_accumulator > 1, 1, n_signals)
-#     temp = s_weights * s_real * n_signals
-#     n_accumulator = np.sum(temp, axis=1)  # n_accumulator + temp_sum
-#     if n_signals[4] > 0:
-#         print('hop')
-#     n_signals = np.zeros((5,))
-#
-# s_cd = np.zeros((5, 5))
-# s_dw = np.zeros((5, 5))
-# s_cd_input = np.random.rand(3, 5, 5) * 2 - 1
-# s_cd_output = np.random.rand(3, 5, 5) * 2 - 1
-#
-# s_dw_input = np.random.rand(3, 5, 5) * 2 - 1
-# s_dw_output = np.random.rand(3, 5, 5) * 2 - 1
-#
-# # расстояние до выхода, верно-неверно, еще байда
-# n_parameters = np.random.rand(3, 5) * 2 - 1
-#
-# # считаем cd на все синапсы
-# cd_input = np.transpose(np.transpose(s_cd_input, (0, 2, 1)) *
-#                         n_parameters.reshape((3, 5, 1)), (0, 2, 1))
-# cd_output = s_cd_output * n_parameters.reshape((3, 5, 1))
-# s_cd = np.sum(cd_output, axis=0) + np.sum(cd_input, axis=0)
-#
-# # считаем dw на все синапсы
-# dw_input = np.transpose(np.transpose(s_dw_input, (0, 2, 1)) *
-#                         n_parameters.reshape((3, 5, 1)), (0, 2, 1))
-# dw_output = s_dw_output * n_parameters.reshape((3, 5, 1))
-# s_dw = np.sum(dw_output, axis=0) + np.sum(dw_input, axis=0)
-# s_dw = s_dw * s_real
-#
-# # получение новых значений реальности s_real
-# temp_cd_greater = np.where(s_cd > 0.8, 1, 0)
-# temp_cd_less = np.where(s_cd < 0.2, 0, 1)
-# s_real_inverse = s_real * (-1) + 1
-# s_real = s_real + s_real_inverse * temp_cd_greater
-# s_real = s_real * temp_cd_less
-# # обнуляем веса удаленных синапсов
-# s_weights = s_weights * s_real
-#
-# # получение новых весов s_weights
-# s_weights = s_weights + s_dw * 0.5
-
 
 class Net:
     def __init__(self, neurons_number=5, input_neurons=None, output_neuron=4, genome=None, parameters_number=3):
@@ -85,29 +13,30 @@ class Net:
         if genome is not None:
             self.genome = genome
             self.parameters_number = len(genome['cd']['input'])
-            self.s_cd_input = None
-            self.s_cd_output = None
-            self.s_dw_input = None
-            self.s_dw_output = None
+            self.s_cd_input = np.array(genome['cd']['input'])
+            self.s_cd_output = np.array(genome['cd']['output'])
+            self.s_dw_input = np.array(genome['dw']['input'])
+            self.s_dw_output = np.array(genome['dw']['output'])
             raise
         else:
             self.parameters_number = parameters_number
-            self.s_cd_input = np.random.rand(self.parameters_number, self.neurons_number, self.neurons_number) * 2 - 1
-            self.s_cd_output = np.random.rand(self.parameters_number, self.neurons_number, self.neurons_number) * 2 - 1
-            self.s_dw_input = np.random.rand(self.parameters_number, self.neurons_number, self.neurons_number) * 2 - 1
-            self.s_dw_output = np.random.rand(self.parameters_number, self.neurons_number, self.neurons_number) * 2 - 1
+            self.s_cd_input = np.random.rand(self.parameters_number) * 2 - 1
+            self.s_cd_output = np.random.rand(self.parameters_number) * 2 - 1
+            self.s_dw_input = np.random.rand(self.parameters_number) * 2 - 1
+            self.s_dw_output = np.random.rand(self.parameters_number) * 2 - 1
         self.n_accumulator = np.zeros((self.neurons_number,))
+        self.n_activation_history = np.zeros((self.neurons_number, BUFFER_SIZE))
         self.n_signals = np.zeros((self.neurons_number,))
         self.s_weights = np.zeros((self.neurons_number, self.neurons_number))
         self.s_signals = np.zeros((self.neurons_number, self.neurons_number))
         self.s_real = np.zeros((self.neurons_number, self.neurons_number))
         self.s_cd = np.zeros((self.neurons_number, self.neurons_number))
         self.s_dw = np.zeros((self.neurons_number, self.neurons_number))
-        # расстояние до входа, правильность*расстояние, история активаций
-        self.n_parameters = np.zeros((self.parameters_number, self.neurons_number))  # * 2 - 1
+        # расстояние до входа, правильность*расстояние, история активаций (mock)
+        self.n_parameters = np.zeros((self.neurons_number, self.parameters_number))  # * 2 - 1
         for n in self.input_neurons:
-            self.n_parameters[0, n] = 0
-        self.n_parameters[0, self.output_neuron] = 1
+            self.n_parameters[n, 0] = 0
+        self.n_parameters[self.output_neuron, 0] = 1
 
     def tick(self, reaction=0, learning=True):
         out_signal = 0
@@ -116,26 +45,31 @@ class Net:
         self.n_accumulator = np.sum(temp, axis=1)  # n_accumulator + temp_sum
         if self.n_signals[self.output_neuron] > 0:
             out_signal = 1
-        self.n_signals = np.zeros((self.neurons_number,))
         if learning:
-            self.compute_cd()
-            self.compute_dw()
+            self.update_parameters()  # reaction
+            self.compute_cd()  # reaction
+            self.compute_dw()  #
             self.check_real()
             self.move_weights()
+        self.n_signals = np.zeros((self.neurons_number,))
         return out_signal
 
+    def update_parameters(self):
+        self.n_activation_history = np.roll(self.n_activation_history, shift=1, axis=0)
+        self.n_activation_history[0] = self.n_signals
+        pass
+
     def compute_cd(self):
-        cd_input = np.transpose(np.transpose(self.s_cd_input, (0, 2, 1)) *
-                                self.n_parameters.reshape((self.parameters_number, self.neurons_number, 1)), (0, 2, 1))
-        cd_output = self.s_cd_output * self.n_parameters.reshape((self.parameters_number, self.neurons_number, 1))
-        self.s_cd = np.sum(cd_output, axis=0) + np.sum(cd_input, axis=0)
+        cd_input = np.sum(self.s_cd_input * self.n_parameters, axis=1)
+        cd_output = np.sum(self.s_cd_output * self.n_parameters, axis=1)
+        self.s_cd = cd_input + cd_output.reshape((self.neurons_number, 1))
+        np.fill_diagonal(self.s_cd, 0)
 
     def compute_dw(self):
-        dw_input = np.transpose(np.transpose(self.s_dw_input, (0, 2, 1)) *
-                                self.n_parameters.reshape((self.parameters_number, self.neurons_number, 1)), (0, 2, 1))
-        dw_output = self.s_dw_output * self.n_parameters.reshape((self.parameters_number, self.neurons_number, 1))
-        self.s_dw = np.sum(dw_output, axis=0) + np.sum(dw_input, axis=0)
-        self.s_dw = self.s_dw * self.s_real
+        dw_input = np.sum(self.s_dw_input * self.n_parameters, axis=1)
+        dw_output = np.sum(self.s_dw_output * self.n_parameters, axis=1)
+        self.s_dw = dw_input + dw_output.reshape((self.neurons_number, 1))
+        np.fill_diagonal(self.s_dw, 0)
 
     def check_real(self):
         # получение новых значений реальности s_real
@@ -195,4 +129,27 @@ def test():
     assert res == [0, 0, 0, 1, 0, 0, 0, 0]
 
 
+BUFFER_SIZE = 10
 test()
+
+a = np.zeros((BUFFER_SIZE, 3))
+b = np.array([1, 0, 1])
+a = np.array([[0, 0, 0],
+              [1, 0, 0],
+              [0, 1, 1],
+              [1, 0, 0],
+              [0, 1, 1],
+              [1, 1, 1],
+              [1, 0, 1]])
+print(a)
+a = np.roll(a, shift=1, axis=0)
+print(a)
+# c = np.dot(a.T, a)
+# print(c)
+# a = np.zeros((3, 2))
+# print(a)
+# a = np.array([0, 1, 2, 3, 4])
+# b = np.array([7, 8, 9, 0, 1])
+# print(a + b.reshape((5, 1)))
+
+
