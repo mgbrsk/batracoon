5801258b7b84651a06e03af3f33aece27a9a68fa
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2021-05-25 17:04:38 +0500

    Update test.py

--- sasha/test.py
+++ sasha/test.py
@@ -73,124 +73,16 @@ class Neuron:
 
     # получение желаемой истории из выходных нейронов, какую историю активаций им хочется
     def get_right_reference(self):
-        # если входной (нет входных весов - не обучается) или выходной (нет выходных нейронов) - ничего не делаем
-        if self.is_input or self.is_output:
-            return
-        # берем эталонную историю выходных нейронов и записываем ее к себе
-        for n in self.output_slots:
-            self.references_from_right.append([n.w_pos, n.w_neg])
+        pass
 
     # подсчет консолидированной эталонной истории, чтобы угодить как можно большим выходным нейронам
     def calculate_wanting(self, y_true=None):
-        """
-        :param y_true: история правильных ответов - для выходного нейрона, если не выходной то не учитывается
-        :return:
-        """
-        # если входной то ничего не делаем
-        if self.is_input:
-            self.references_from_right = []
-            return
-        # если выходной то эталонная история для него - история правильных ответов датасета
-        if self.is_output:
-            '''
-            a = np.array([0, 0, 1, 2, 3, 4, 5])
-            b = np.array([1, 2, 3, 4, 5, 0, 0])
-            a = np.roll(a, -2)
-            print(a[:-2])
-            print(b[:-2])
-            '''
-            temp_history = np.array(self.history)
-            temp_history = np.roll(temp_history, -2)
-            # temp_history = temp_history[:-2]
-            # temp_true = np.array(y_true)[:-2]
-            temp_true = np.array(y_true)
-            # temp = np.array(y_true) - np.array(self.history)
-            temp = temp_true - temp_history
-            self.w_pos = np.where(temp > 0, 1, 0)
-            self.w_neg = np.where(temp < 0, 1, 0)
-            self.references_from_right = []
-            return
-
-        if len(self.references_from_right) == 1:  # нейроны среднего слоя
-            self.w_pos = self.references_from_right[0][0]
-            self.w_neg = self.references_from_right[0][1]
-            self.w_pos = list(np.roll(np.array(self.w_pos), 1))
-            self.w_neg = list(np.roll(np.array(self.w_neg), 1))
-            self.references_from_right = []
-            return
-        raise
-        # positive = [[en] + x[0] for en, x in enumerate(self.references_from_right)]  # позитивные требования
-        # negative = [[en] + x[1] for en, x in enumerate(self.references_from_right)]
-        #
-        # # !!! возможно стоит еще сравнить с текущей историей
-        # temp = list(combinations(positive + negative, r=2))  # все комбинации
-        # temp = [[x[0][1:], x[1][1:]] for x in temp if x[0][0] != x[1][0]]  # очистка комбинаций от показателей знака
-        # temp = np.array(temp)   # очищенные комбинации историй
-        # func = lambda x: x[0] * x[1]
-        # t = np.apply_along_axis(func, 1, temp)  # произведения комбинаций историй
-        #
-        # temp_sum = np.sum(t, axis=1)  # рейтинг похожестей
-        # indexes_of_max = np.where(temp_sum == temp_sum.max())
-        # random_index = random.choice(indexes_of_max)
-        # reference = list(t[random_index])
-        # temp_dif = np.array(reference) - np.array(self.history)
-        # self.w_pos = np.where(temp_dif > 0, 1, 0)
-        # self.w_neg = np.where(temp_dif < 0, 1, 0)
-        # self.references_from_right = []
+        pass
 
     # метод изменения входных весов нейрона
     def move_weights(self):
         # если входной ничего не делаем, нет входных весов
-        if self.is_input:
-            return
-        # идем по всем входным нейронам
-        for en, n in enumerate(self.input_slots):
-            print(self.number, n.number)
-            print(np.array(n.history))
-            print(np.array(self.w_pos))
-            pos_coef1 = np.array(n.history) * np.array(self.w_pos)
-            pos_coef2 = np.array(n.history) == np.array(self.w_pos)
-            pos_coef2 = pos_coef2 * 1
-            # if np.sum(self.w_pos) <= 4:
-            #     continue
-
-            pos_coef = pos_coef1 * 0.2 + pos_coef2 * 0.8
-
-            # нормируем от 0 до 1 - это степень похожести
-            pos_coef = np.sum(pos_coef * 1) / pos_coef.shape[0]
-            # если у нас в истории необходимых положительных импульсов ничего нет -
-            # то считаем степень похожести как 0 (чтобы не двигать вес)
-            # if (not np.array(self.pos_reference_left).any()):
-            #     # (not np.array(n.history).any()):  #  and (not np.array(self.pos_reference_left).any())
-            #     pos_coef = 0
-
-            neg_coef1 = np.array(n.history) * np.array(self.w_neg)
-            neg_coef2 = np.array(n.history) == np.array(self.w_neg)
-            neg_coef2 = neg_coef2 * 1
-            # if np.sum(self.w_neg) <= 4:
-            #     continue
-            neg_coef = neg_coef1 * 0.2 + neg_coef2 * 0.8
-
-            # то же самое и для истории отрицательных необходимых импульсов
-            neg_coef = np.sum(neg_coef * 1) / neg_coef.shape[0]
-            # if (not np.array(self.neg_reference_left).any()):
-            #     # (not np.array(n.history).any()):  #  and (not np.array(self.neg_reference_left).any())
-            #     neg_coef = 0
-
-            # если отрицательная и положительная похожесть одинакова то ничего не делаем
-            if pos_coef == neg_coef:
-                continue
-            # если положительная похожесть больше чем отрицательная то двигаем вес в сторону его увеличения,
-            # если отрицательная больше - то двигаем вес в сторону его уменьшения
-            if pos_coef > neg_coef:
-                self.weights[en] += pos_coef * 0.5
-            if pos_coef < neg_coef:
-                self.weights[en] -= neg_coef * 0.5
-            # жестко ограничиваем веса от -1 до 1
-            if self.weights[en] > 1:
-                self.weights[en] = 1
-            if self.weights[en] < -1:
-                self.weights[en] = -1
+        pass
 
 
 """
@@ -326,5 +218,6 @@ def main():
                     res.append(str(k))
             print(','.join(res), file=f)
             # print(','.join(res))
+
 
 main()
