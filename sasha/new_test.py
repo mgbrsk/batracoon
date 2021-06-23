27d186773398d0971add1337296581a78ff21912
fizteh95 <causeLove95@yandex.ru>
2021-06-23 22:47:05 +0500

    Вставки нейронов.

--- sasha/new_test.py
+++ sasha/new_test.py
@@ -58,8 +58,24 @@ def truth_table(inputs_count: int):
     return np.array(X), np.array(y)
 
 
+def split_x(x_train, number_of_splits=2):
+    length = x_train.shape[0]
+    parts = length // number_of_splits
+    last = length % number_of_splits
+    print(parts * number_of_splits + last == length)
+    result = []
+    for i in range(parts):
+        first_index = i * number_of_splits
+        second_index = (i + 1) * number_of_splits
+        temp = x_train[first_index:second_index, :]
+        result.append(temp)
+    if last:
+        result.append(x_train[second_index:, :])
+    return result
+
+
 # создаем таблицу истинности
-x, y = truth_table(3)
+x, y = truth_table(2)
 print('X:')
 print(x)
 print('\n')
@@ -68,99 +84,138 @@ print(y)
 print('\n')
 
 
-# словарь, в который будем сохранять все узлы сети
+
+x_train = split_x(x, number_of_splits=2)
+for i in x_train:
+    print(i)
+
+
+
+# # словарь, в который будем сохранять все узлы сети
+# net = {}
+#
+# # из таблицы истинности создаем входные нейроны с заданной историей (колонки Х)
+# for i in range(x.shape[1]):
+#     net[str(i)] = {'number': i, 'input_numbers': [], 'input_weights': [], 'history': x[:, i]}
+#
+# print(x.shape)
+#
+# # начальное качество, которое будем требовать с новых узлов
+# prev_accuracy = 0.2
+#
+# # пока не получили требуемое качество сети
+# while True:
+#     # пока не получим ненулевую новую историю активаций узла
+#     while True:
+#         # берем рандомное число (два или более), со сколькими входными узлами будем работать
+#         random_count = random.randint(2, len(net))
+#         # получаем набор осмысленных весов для данного числа узлов
+#         mwc = make_weights_combinations(random_count)
+#         # инициализурем чтобы линтер не ругался
+#         temp = None
+#         # максимальное число комбинаций весов
+#         max_number = len(mwc) ** random_count
+#         # инициализация чтобы не ругался линтер
+#         weight_combination = None
+#         rand_neurons = None
+#         count = 0
+#         # пока не получим норм комбинацию весов (которая не будет давать в любом случае нули)
+#         while True:
+#             # собираем рандомную комбинацию весов из доступных вариантов
+#             temp = [random.choice(mwc) for x in range(random_count)]
+#             # если эта комбинация не вся состоит из нулевых и отрицательных весов
+#             if filter_tuple_weights(temp):
+#                 # у нас есть новая норм комбинация входных весов!
+#                 weight_combination = temp
+#                 break
+#             # если не повезло то увеличиваем счетчик
+#             count += 1
+#             # и если таки мы сидим в этом цикле больше раз чем всего комбинаций есть, то выходим из цикла с пустым weight_combination
+#             if count >= max_number:
+#                 break
+#         # если так ничего не нашли, то еще раз шерстим случайные комбинации весов
+#         if not weight_combination:
+#             continue
+#
+#         # берем random_count узлов из сети
+#         rand_neurons = random.sample(net.keys(), random_count)
+#
+#         # преобразуем комбинацию входных весов в массив нампи
+#         weight_combination = np.array(weight_combination)
+#         # создаем матрицу-вектор (вертикальную)
+#         weight_combination = weight_combination.reshape((len(weight_combination), 1))
+#
+#         # создаем матрицу входных историй активаций для данного узла
+#         np_neurons = np.array([net[x]['history'] for x in rand_neurons])
+#         # а также переводим все истории в матричный вид нампая
+#         all_np_neurons = np.array([net[x]['history'] for x in list(net.keys())])
+#
+#         # получаем результат для данного узла - умножаем истории на веса, суммируем и применяем пороговую функцию
+#         temp = weight_combination * np_neurons
+#         temp = temp.sum(axis=0)
+#         temp = np.where(temp >= 1, 1, 0)
+#         # если получилась ненулевая история и ее еще нет в уже имеющихся историях
+#         if temp.any() and not (temp == all_np_neurons).all(axis=1).any():
+#             # выходим из цикла и идем дальше
+#             break
+#         else:
+#             # иначе выбираем заново веса и нейроны
+#             continue
+#
+#     # считаем текущее качество последнего узла
+#     current_accuracy = (temp == np.array(y)).sum() / len(y)
+#     print(current_accuracy, prev_accuracy)
+#     # если качество больше целевого
+#     if current_accuracy >= prev_accuracy:
+#         # записываем новый получившийся узел в сеть
+#         last_number = int(list(net.keys())[-1])
+#         new_node = {'number': last_number + 1, 'input_numbers': rand_neurons,
+#                     'input_weights': weight_combination, 'history': temp}
+#         net[str(last_number + 1)] = new_node
+#         # увеличиваем целевое качество
+#         prev_accuracy += 0.025
+#         # если уже получили стопроцентное попадание на последнем узле
+#         if current_accuracy >= 1.0:
+#             # выходим из главного цикла, закончили
+#             break
+#         # если недостаточное качество то идем на еще один круг
+#         continue
+#     # если качество хуже целевого
+#     else:
+#         # заново выбираем количество входных узлов, веса, и входные нейроны
+#         continue
+#
+#
+# pprint(net)
+
+
+# class Node:
+#     def __init__(self, number, input_nodes=None, weights=None, history=None, is_output=False):
+#         pass
+#
+#     def append_link(self):
+#         pass
+#
+#     def append_node(self):
+#         pass
+#
+#     def forward_move(self):
+#         pass
+
+
+
+def get_last(net):
+    numbers = list(net.keys())
+    numbers = map(lambda x: str(x), numbers)
+    return max(numbers)
+
 net = {}
 
-# из таблицы истинности создаем входные нейроны с заданной историей (колонки Х)
-for i in range(x.shape[1]):
-    net[str(i)] = {'number': i, 'input_numbers': [], 'input_weights': [], 'history': x[:, i]}
+for i in x.shape[1]:
+    temp_node = dict(number=i, input_nodes=[], weights=[], history=[], is_output=False)
+    net[str(i)] = temp_node
 
-print(x.shape)
-
-# начальное качество, которое будем требовать с новых узлов
-prev_accuracy = 0.2
-
-# пока не получили требуемое качество сети
-while True:
-    # пока не получим ненулевую новую историю активаций узла
-    while True:
-        # берем рандомное число (два или более), со сколькими входными узлами будем работать
-        random_count = random.randint(2, len(net))
-        # получаем набор осмысленных весов для данного числа узлов
-        mwc = make_weights_combinations(random_count)
-        # инициализурем чтобы линтер не ругался
-        temp = None
-        # максимальное число комбинаций весов
-        max_number = len(mwc) ** random_count
-        # инициализация чтобы не ругался линтер
-        weight_combination = None
-        rand_neurons = None
-        count = 0
-        # пока не получим норм комбинацию весов (которая не будет давать в любом случае нули)
-        while True:
-            # собираем рандомную комбинацию весов из доступных вариантов
-            temp = [random.choice(mwc) for x in range(random_count)]
-            # если эта комбинация не вся состоит из нулевых и отрицательных весов
-            if filter_tuple_weights(temp):
-                # у нас есть новая норм комбинация входных весов!
-                weight_combination = temp
-                break
-            # если не повезло то увеличиваем счетчик
-            count += 1
-            # и если таки мы сидим в этом цикле больше раз чем всего комбинаций есть, то выходим из цикла с пустым weight_combination
-            if count >= max_number:
-                break
-        # если так ничего не нашли, то еще раз шерстим случайные комбинации весов
-        if not weight_combination:
-            continue
-
-        # берем random_count узлов из сети
-        rand_neurons = random.sample(net.keys(), random_count)
-
-        # преобразуем комбинацию входных весов в массив нампи
-        weight_combination = np.array(weight_combination)
-        # создаем матрицу-вектор (вертикальную)
-        weight_combination = weight_combination.reshape((len(weight_combination), 1))
-
-        # создаем матрицу входных историй активаций для данного узла
-        np_neurons = np.array([net[x]['history'] for x in rand_neurons])
-        # а также переводим все истории в матричный вид нампая
-        all_np_neurons = np.array([net[x]['history'] for x in list(net.keys())])
-
-        # получаем результат для данного узла - умножаем истории на веса, суммируем и применяем пороговую функцию
-        temp = weight_combination * np_neurons
-        temp = temp.sum(axis=0)
-        temp = np.where(temp >= 1, 1, 0)
-        # если получилась ненулевая история и ее еще нет в уже имеющихся историях
-        if temp.any() and not (temp == all_np_neurons).all(axis=1).any():
-            # выходим из цикла и идем дальше
-            break
-        else:
-            # иначе выбираем заново веса и нейроны
-            continue
-
-    # считаем текущее качество последнего узла
-    current_accuracy = (temp == np.array(y)).sum() / len(y)
-    print(current_accuracy, prev_accuracy)
-    # если качество больше целевого
-    if current_accuracy >= prev_accuracy:
-        # записываем новый получившийся узел в сеть
-        last_number = int(list(net.keys())[-1])
-        new_node = {'number': last_number + 1, 'input_numbers': rand_neurons,
-                    'input_weights': weight_combination, 'history': temp}
-        net[str(last_number + 1)] = new_node
-        # увеличиваем целевое качество
-        prev_accuracy += 0.025
-        # если уже получили стопроцентное попадание на последнем узле
-        if current_accuracy >= 1.0:
-            # выходим из главного цикла, закончили
-            break
-        # если недостаточное качество то идем на еще один круг
-        continue
-    # если качество хуже целевого
-    else:
-        # заново выбираем количество входных узлов, веса, и входные нейроны
-        continue
-
+last_number = get_last(net)
+net[str(last_number + 1)] = dict(number=last_number + 1, input_nodes=[], weights=[], history=[], is_output=True)
 
 pprint(net)
